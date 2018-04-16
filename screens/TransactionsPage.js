a1915b7dc0b8ab818647152dfb3138e6c6796c61
guildenstern <alessiosaltarin@it.ibm.com>
2018-04-16 12:16:38 +0200

    Dettaglio Operazioni

--- screens/TransactionsPage.js
+++ screens/TransactionsPage.js
@@ -7,7 +7,7 @@
 
 import React, {Component} from 'react';
 
-import {Button, H2, List, ListItem, Text, Thumbnail} from "native-base";
+import {Body, Button, Grid, H2, Left, List, ListItem, Right, Row, Text, Thumbnail} from "native-base";
 import type {NavigationScreenProp, NavigationState} from "react-navigation";
 import AweTabsLayout from "../components/tabslayout";
 import Styles from "../styles";
@@ -24,6 +24,37 @@ type Props = {
     card: CreditCard
 };
 
+type Operation = {
+    date: string,
+    subject: string,
+    location: string,
+    amount: number,
+    isNew: boolean
+}
+
+const operations: Array<Operation> = [
+    {
+        date: 'Ieri alle 07.34',
+        subject: 'Certificato di residenza',
+        location: 'Comune di Gallarate',
+        amount: -20.02,
+        isNew: true
+    },
+    {
+        date: '22/12/2018 - 07:34',
+        subject: 'Rimborso TARI 2012',
+        location: 'Comune di Gallarate',
+        amount: -20.02,
+        isNew: false
+    },
+    {
+        date: '17/12/2018 - 22:23',
+        subject: 'Ristorante I Pini',
+        location: 'Busto Arsizio',
+        amount: -134.00,
+        isNew: false
+    }];
+
 class TransactionsPage extends Component<Props>
 {
     render()
@@ -31,28 +62,62 @@ class TransactionsPage extends Component<Props>
         const {navigate} = this.props.navigation;
         const {params} = this.props.navigation.state;
         const card: CreditCard = params ? params.card : "";
-        const TITLE: string = 'Transazioni carta ';
+        const TITLE: string = 'OPERAZIONI';
 
         return (
 
             <AweTabsLayout title={TITLE}>
-                <Thumbnail source={card.image}/>
-                <H2 style={Styles.titleStyle}>{TITLE}</H2>
-                <Text>{card.text}</Text>
-                <List style={{marginTop: 20}}>
-                    <ListItem>
-                        <Text>12.01.18 - Pranzo da Babette - Eur.120,00</Text>
-                    </ListItem>
-                    <ListItem>
-                        <Text>10.01.18 - Bonifico Marozzi - Eur.1.120,00</Text>
-                    </ListItem>
-                    <ListItem>
-                        <Text>10.01.18 - Esselunga Supermkt - Eur.25,50</Text>
-                    </ListItem>
-                </List>
-                <Button style={{marginTop: 40}} success title="Back" onPress={() => navigate('Home')}>
-                    <Text>Torna</Text>
-                </Button>
+
+                <Grid>
+                    <Row size={1}>
+                        <Text note>{card.text} - {card.number}</Text>
+                    </Row>
+                    <Row size={1}>
+                        <Left>
+                            <H2 style={Styles.titleStyle}>{TITLE}</H2>
+                        </Left>
+                        <Right>
+                            <Text>Totale EUR</Text>
+                        </Right>
+                    </Row>
+
+                    <Row size={4}>
+
+                        <List style={{marginTop: 20}} dataArray={operations} renderRow={item =>
+
+                            <ListItem>
+                                <Body>
+                                <Grid>
+                                    <Row>
+                                        <Text note>{item.date}</Text>
+                                    </Row>
+                                    <Row>
+                                        <Left>
+                                            <Text>{item.subject}</Text>
+                                        </Left>
+                                        <Right>
+                                            <Text>{item.amount}</Text>
+                                        </Right>
+                                    </Row>
+                                    <Row>
+                                        <Text note>{item.location}</Text>
+                                    </Row>
+                                </Grid>
+                                </Body>
+                            </ListItem>
+                        }
+
+                        />
+
+                    </Row>
+                    <Row size={1}>
+                        <Button style={{marginTop: 40}} success title="Back" onPress={() => navigate('Home')}>
+                            <Text>Torna</Text>
+                        </Button>
+                    </Row>
+                </Grid>
+
+
             </AweTabsLayout>
 
         );
